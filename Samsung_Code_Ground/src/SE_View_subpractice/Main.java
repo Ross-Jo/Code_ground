b1a594390db1c522fb8a80081dbf2c22bd0f36eb
Bubryong Jo <jby0711823@naver.com>
2017-10-15 23:00:39 +0900

    se

--- Samsung_Code_Ground/src/SE_View_subpractice/Main.java
+++ Samsung_Code_Ground/src/SE_View_subpractice/Main.java
@@ -29,25 +29,30 @@ public class Main {
 	public static int isGood(int idx){
 		int cnt = 0;
 		int ret = 987654321;
-		for(int i=0; i<d.length; i++){
-			if(idx+d[i]>=0 && idx+d[i] <L && c[idx+d[i]][idx]!=-987654321 && c[idx+d[i]][idx]>=1){
+		
+		for(int i=0; i<d.length; i++){ // 좌로 2채, 우로 2채의 건물에 대하여 모두 체크
+			if(idx+d[i]>=0 && idx+d[i] <L && c[idx+d[i]][idx]!=-987654321 && c[idx+d[i]][idx]>=1){ // 인덱스 범위 체크, 이전 계산여부 체크
 				cnt++;
 				ret = Math.min(ret, c[idx+d[i]][idx]);
 			}
-			else if(idx+d[i]>=0 && idx+d[i] <L && c[idx+d[i]][idx]==-987654321){
+			else if(idx+d[i]>=0 && idx+d[i] <L && c[idx+d[i]][idx]==-987654321){ // 이전에 계산을 하지 않은 경우
 				
-				if(buildings[idx+d[i]]>buildings[idx]) return -1;
+				if(buildings[idx+d[i]]>buildings[idx]) return -1; // 만약 (주어진 범위 내에서) 자신보다 더 큰 빌딩을 만났을 경우, 함수식을 종료함.
 				
-				c[idx+d[i]][idx] = buildings[idx] - buildings[idx+d[i]];
+				c[idx+d[i]][idx] = buildings[idx] - buildings[idx+d[i]]; // idx+d[i] 번째 빌딩과, idx 빌딩의 높이 차이. 기준은 idx빌당.
 				c[idx][idx+d[i]] = -c[idx+d[i]][idx];
 				
-				if(c[idx+d[i]][idx]>=1){
+				/* 점화식을 사용할 때는 해당 점화식의 의미를 명확히 규정하고 사용하자. 그렇게 하지 않았을 경우 의도치 않은 결과가 나올 가능성이 다분하다. 
+				 */
+				
+				if(c[idx+d[i]][idx]>=1){ // 해당 건물과 비교시 조망이 보이는 (층이 있는)지 체크
 					cnt++;
 					ret = Math.min(ret, c[idx+d[i]][idx]);
 				}
 			}
 		}
-		if(cnt==4) return ret;
+		
+		if(cnt==4) return ret; // 좌로 2채, 우로 2채 비교 했는데 다 조망이 보인다면
 		else return -1;
 	}
 	
