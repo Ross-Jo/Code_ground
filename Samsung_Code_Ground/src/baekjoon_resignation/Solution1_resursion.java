02890f3ae3fadbaa288ee4ded64d94e30e9c9693
Bubryong Jo <jby0711823@naver.com>
2017-10-18 22:21:18 +0900

    bj

---
+++ Samsung_Code_Ground/src/baekjoon_resignation/Solution1_resursion.java
@@ -0,0 +1,36 @@
+package baekjoon_resignation;
+
+import java.util.*;
+
+public class Solution1_resursion {
+	static int[] d = new int[20];
+	static int[] t = new int[20];
+	static int[] p = new int[20];
+	static int n;
+	static int ans = 0;
+
+	static void go(int x, int sum) {
+		if (x == n) {
+			if (ans < sum)
+				ans = sum;
+			return;
+		}
+		if (x > n) {
+			return;
+		}
+		go(x + 1, sum);
+		go(x + t[x], sum + p[x]);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			t[i] = sc.nextInt();
+			p[i] = sc.nextInt();
+		}
+		go(0, 0);
+		System.out.println(ans);
+		sc.close();
+	}
+}
