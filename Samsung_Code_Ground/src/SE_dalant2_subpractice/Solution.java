36531084a6a01885a60251f9c641b755978d2684
Bubryong Jo <jby0711823@naver.com>
2017-10-18 11:33:12 +0900

    se

--- Samsung_Code_Ground/src/SE_dalant2_subpractice/Solution.java
+++ Samsung_Code_Ground/src/SE_dalant2_subpractice/Solution.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 //비고 : SEexpert 타인 코드 참고
 public class Solution {
 	public static int N; 
-	public static int P;
+	public static int P; 
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(".\\src\\SE_dalant2_subpractice\\input.txt"));
@@ -21,10 +21,11 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			P = Integer.parseInt(st.nextToken());
 			
-			for(int i=P; i>0; --i){
+			for(int i=P; i>0; --i){ // 특정 숫자를 몇개의 부분으로 나눴을 때, 가장 비슷한 크기로 나누는 것이 나눈 부분의 곱이 최대가 된다.
 				result *= N/i;
 				N -= N/i;
 			}
+			// 참고 : https://math.stackexchange.com/questions/125065/partitioning-a-natural-number-n-in-order-to-get-the-maximum-product-sequence-o
 			
 			System.out.println("#"+t+" "+result);
 		}
